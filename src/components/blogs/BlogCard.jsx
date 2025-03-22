525881f7d6b24d8d8c934509211a670107792ae3
rajib18197 <rajib18197@gmail.com>
2025-03-22 22:20:03 +0600

    adjust minor things

--- src/components/blogs/BlogCard.jsx
+++ src/components/blogs/BlogCard.jsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { useState } from "react";
-import { motion } from "framer-motion";
 import { Calendar, Clock, ArrowRight } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardFooter } from "@/components/ui/card";
@@ -10,21 +9,7 @@ import Link from "next/link";
 export default function BlogCard({ post, index }) {
   const [isHovered, setIsHovered] = useState(false);
 
-  const itemVariants = {
-    hidden: { opacity: 0, y: 20 },
-    visible: {
-      opacity: 1,
-      y: 0,
-      transition: { duration: 0.5 },
-    },
-  };
-
   return (
-    // <motion.div
-    //   variants={itemVariants}
-    //   onMouseEnter={() => setIsHovered(true)}
-    //   onMouseLeave={() => setIsHovered(false)}
-    // >
     <Card className="h-full overflow-hidden border-none shadow-md hover:shadow-xl transition-all duration-300 group">
       <div className="relative">
         <div className="aspect-[16/9] overflow-hidden">
@@ -64,24 +49,12 @@ export default function BlogCard({ post, index }) {
         <p className="text-muted-foreground mb-4 line-clamp-3">
           {post.excerpt}
         </p>
-
-        {/* <div className="flex items-center">
-            <div className="w-8 h-8 rounded-full overflow-hidden mr-3">
-              <img
-                src={post.authorImage || "/placeholder.svg"}
-                alt={post.author}
-                className="w-full h-full object-cover"
-              />
-            </div>
-            <span className="font-medium text-sm">{post.author}</span>
-          </div> */}
       </CardContent>
 
       <CardFooter className="p-6 pt-0">
         <Link href={`/blog/${post?._id}`}>
           <Button
             variant="ghost"
-            // className="p-0 h-auto font-medium group/btn hover:bg-transparent"
             style={{
               background: `hsl(221.2 83.2% 53.3%)`,
               color: "white",
@@ -94,6 +67,5 @@ export default function BlogCard({ post, index }) {
         </Link>
       </CardFooter>
     </Card>
-    // </motion.div>
   );
 }
